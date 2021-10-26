fa6378e9559f82ff063432d31aeb08918ce96800
linbossgebest <809621397@qq.com>
2021-10-26 18:27:17 +0800

    update

---
+++ CoreTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+
+namespace CoreTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder => builder
+                .UseStartup<Startup>()
+                .ConfigureServices(svcs => svcs.AddSingleton<IFoo, Foo>()))
+            .Build()
+            .Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) => services.AddSingleton<IBar, Bar>();
+        public void Configure(IApplicationBuilder app, IFoo foo, IBar bar)
+        {
+            Console.WriteLine(foo != null);
+            Console.WriteLine(bar != null);
+        }
+    }
+
+    public interface IFoo { }
+    public class Foo : IFoo { }
+    public interface IBar { }
+    public class Bar : IBar { }
+}
---
+++ PipeLine/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace PipeLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static RequestDelegate Middleware1(RequestDelegate next) => async context =>
+            {
+                await context.Response.WriteAsync("Hello");
+                await next(context);
+            };
+            static RequestDelegate Middleware2(RequestDelegate next) => async context =>
+            {
+                await context.Response.WriteAsync(" World!");
+                await next(context);
+            };
+            static RequestDelegate Middleware3(RequestDelegate next)
+            {
+               return context => test1(context);
+                //RequestDelegate requestDelegate = test1;
+                //return requestDelegate;
+                //return async context =>
+                //{
+                //    await context.Response.WriteAsync(" World!");
+                //};
+            }
+            // static RequestDelegate Middleware3(RequestDelegate next)
+            // {
+            //     return  test1(context);
+            //}
+            static RequestDelegate test(HttpContext context)
+            {
+                return (context) => test1(context);
+            }
+            static async Task test1(HttpContext context)
+            {
+                await context.Response.WriteAsync("hello1188");
+            }
+
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder => builder.Configure(app => app
+                .Use(Middleware1)
+                .Use(Middleware2)
+                .Use(Middleware3)))
+            .Build()
+            .Run(); 
+        }
+
+        
+
+    }
+}
